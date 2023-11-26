c781a2d2d50064abc887cca55d8206150c230c1a
shermatovelbek <shermatovelbek772@gmail.com>
2023-11-26 21:12:06 +0500

    javascript fayllar ochildi

---
+++ js/java.js
@@ -0,0 +1,61 @@
+// let a = 5;
+// // assigment operators
+// a **= 3;
+// console.log(a)
+
+
+
+// let a = +prompt("birinchi insonni yoshini kiriting ...");
+// let b = +prompt("ikkinchi insonni yoshini kiriting ...");
+
+
+// if (a > b){
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+
+        // 1-misol
+
+// let a = +prompt("birinchi sonni kiriting");
+// a*2;
+// let b = +prompt("ikkinchi sonni kiriting");
+// b*2
+
+
+// if (a > b) {
+//     console.log(b)
+// }
+// if(a < b) {
+//     console.log(b)
+// }
+
+        // 2-misol
+
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchinchi sonni kiriting");
+
+//   if (a > b) {
+//     console.log(a);
+//   } else{
+//     b
+//   }
+//   if(b > c) {
+//     console.log(b);
+
+//   } else {
+//     console.log(c)
+//   }
+
+
+
+          // 3-misol
+    
+  let juft = nameprompt("sonni kiriting");
+
+  if (a % 2 == 0) {
+    
+  }
+  console.log(juft);
